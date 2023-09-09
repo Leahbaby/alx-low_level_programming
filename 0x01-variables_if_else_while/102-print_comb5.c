d108f15b9d0149b91adcb1fddcbae3967e54cbbd
Leahbaby <umehleah@gmail.com>
2023-09-08 17:02:29 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Generate pairs of two-digit numbers without repeating digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+if (i != j)
+{
+printf("%02d %02d", i, j);
+
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
