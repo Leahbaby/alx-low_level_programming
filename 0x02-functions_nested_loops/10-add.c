89b12b750e5e9b42fef03436f0891794fa1f3e60
Leahbaby <umehleah@gmail.com>
2023-09-12 12:32:48 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,46 +1,16 @@
 #include "main.h"
 
 /**
- * print_times_table - main function
- * @n: function param
- * Desc: a function that prints n times table starting from 0
- * Return: Void
+ * add - Entry
+ * @a: function parameter
+ * @b: function parameter
+ * Desc:
+ *
+ * Return: Always 0 (Success)
  */
-void print_times_table(int n)
+int add(int a, int b)
 {
-int count;
-int multiplier;
-int res;
+int res = a + b;
 
-if (n >= 0 && n <= 15)
-for (count = 0; count <= n; count++)
-{
-_putchar('0');
-for (multiplier = 1; multiplier <= n; multiplier++)
-{
-_putchar(',');
-_putchar(' ');
-res = count * multiplier;
-
-if (res < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(res + '0');
-}
-else if (res >= 10 && res < 100)
-{
-_putchar(' ');
-_putchar((res / 10) + '0');
-_putchar((res % 10) + '0');
-}
-else
-{
-_putchar((res / 100) + '0');
-_putchar(((res / 10) % 10) + '0');
-_putchar((res % 10) + '0');
-}
-}
-_putchar('\n');
-}
+return (res);
 }
