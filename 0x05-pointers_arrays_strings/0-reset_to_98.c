81036158bffa2f7506ad27b6a7830b7634023b4f
Leahbaby <umehleah@gmail.com>
2023-09-18 15:47:57 -0700

    more function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - resets int to 98.
+ *@n: argument to dereference.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+project
+ Pointers, arrays and strings
