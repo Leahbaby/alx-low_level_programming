daa3e39ad77053f74ab09891f77fb7832788cb3f
Umeh leah <umehleah@gmail.com>
2023-10-10 15:46:26 +0100

    more function

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+
+
+#include "dog.h"
+
+/**
+ * print_dog - prints the properties of "dog"
+ * @d: pointer to object "dog"
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
